d80f635a2114ac1fff0e003b43b5ee4f75b3b65b
nagaraj-devops <147501947+nagaraj-devops@users.noreply.github.com>
2024-07-22 19:41:36 +0530

    Update one_time_mysql_backup.sh

--- one_time_mysql_backup.sh
+++ one_time_mysql_backup.sh
@@ -5,7 +5,7 @@ ENDCOLOR="\e[0m"
 
 git clone https://github.com/nagaraj-devops/backup_shell_scripts.git
 
-echo -e "${GREEN}git clone taken{ENDCOLOR}"
+echo -e "${GREEN}git clone taken ${ENDCOLOR}"
 
 sudo cp backup_shell_scripts/mysql_db_backup_script.sh /usr/local/bin
 sudo cp backup_shell_scripts/capture_mysql_input.sh /usr/local/bin
@@ -13,23 +13,23 @@ sudo cp backup_shell_scripts/capture_mysql_input.sh /usr/local/bin
 sudo chmod +x /usr/local/bin/mysql_db_backup_script.sh
 sudo chmod +x /usr/local/bin/capture_mysql_input.sh
 
-echo -e "${GREEN}copied shell files to bin{ENDCOLOR}"
+echo -e "${GREEN}copied shell files to bin ${ENDCOLOR}"
 
 capture_mysql_input.sh
 
 sudo mv saved_vars.sh /usr/local/bin
 sudo chmod +x /usr/local/bin/saved_vars.sh
 
-echo -e "${GREEN}move saved varibles file to bin{ENDCOLOR}"
+echo -e "${GREEN}move saved varibles file to bin ${ENDCOLOR}"
 
 source /usr/local/bin/saved_vars.sh
 
-echo -e "${GREEN}source the exported variables{ENDCOLOR}"
+echo -e "${GREEN}source the exported variables ${ENDCOLOR}"
 
 mysql_db_backup_script.sh
 
-echo -e "${GREEN}Database backed up and moved to s3 bucket{ENDCOLOR}"
+echo -e "${GREEN}Database backed up and moved to s3 bucket ${ENDCOLOR}"
 
 sudo rm -rf backup_shell_scripts
 
-echo -e "${RED}removed git repo{ENDCOLOR}"
+echo -e "${RED}removed git repo ${ENDCOLOR}"
